c06028d4ab3eae38ba1c130b5a054032a3c0c522
GTS3636 <163864946+GTS3636@users.noreply.github.com>
2024-11-05 12:04:03 -0300

    Delete javascript directory

--- javascript/prog.js
+++
@@ -1,9 +0,0 @@
-let pagin = document.querySelector("body")
-let button = document.getElementById("contato")
-let res_c = document.createElement("div")
-res_c.setAttribute("id", "res_c")
-pagin.appendChild(res_c)
-
-button.addEventListener("click", ()=>{
-
-})
