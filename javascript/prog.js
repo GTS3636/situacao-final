4ad99a2f33012b87f54fae7f07a828f72440b4d2
GUILHERME TOMAZ SILVA <guilherme_silva56@estudante.sesisenai.org.br>
2024-11-05 11:55:56 -0300

    versão alpha

---
+++ javascript/prog.js
@@ -0,0 +1,9 @@
+let pagin = document.querySelector("body")
+let button = document.getElementById("contato")
+let res_c = document.createElement("div")
+res_c.setAttribute("id", "res_c")
+pagin.appendChild(res_c)
+
+button.addEventListener("click", ()=>{
+
+})
